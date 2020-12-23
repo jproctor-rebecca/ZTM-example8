278e4283b0d39d6fba658b8f46e8dfbfe1fec3e8
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 22:17:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# expressions vs statements
+
+iq = 100
+user_age = iq / 5
+  # iq / 5 is an expression because it produces a value
+  # user_age = iq / 5 is a statement because it is an entire line of code that performs an action
+
